1a4dd4f774b3fb7401e08c7aaafc36607d718b0f
johnny1864 <johnny1864@yahoo.com>
2018-12-01 00:48:08 +0000

    Linked to MailChimpapi

--- app.js
+++ app.js
@@ -19,6 +19,44 @@ app.post('/signup', (req, res) => {
     if (!firstName || !lastName || !email) {
         res.redirect('/fail.html');
     }
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ],
+    }
+
+    const postData = JSON.stringify(data);
+
+    const options = {
+        url: 'https://us19.api.mailchimp.com/3.0/lists/9bc0396d61',
+        method: 'POST',
+        headers: {
+            Authorization: 'auth 54470479764ca5173fd4ba99f7570e68-us19'
+        },
+        body: postData
+    }
+
+    request(options, (err, response, body) => {
+        if (err) {
+            res.redirect('/fail.html');
+        }
+        else {
+            if (response.statusCode === 200) {
+                res.redirect('/success.html')
+            }
+            else {
+                res.redirect('/fail.html');
+            }
+        }
+    });
 });
 
 const PORT = process.env.PORT || 8081;
